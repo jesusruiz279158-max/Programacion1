5616cff58cbdacdd8e3be72836c980061f39ec59
Abraham Ruiz <jesus.ruiz279158@potros.itson.edu.mx>
2025-10-14 20:22:38 -0700

    Correción Oráculo

--- Practicas/Oráculo.py
+++ Practicas/Oráculo.py
@@ -1,38 +1,34 @@
 
-def calcular_elemento(anioAc, anioN):
+def calcular_elemento():
 
-    anioAc = int(2025)
     anioN = int(input("Por favor ingrese su año de nacimiento: "))
     ultD = anioN%10
-    if ultD == 0 | 1 :
+
+    if ultD == 0 or ultD == 1:
         return "Metal"
-    elif ultD == 2 | 3 :
+    elif ultD == 2 or ultD== 3:
         return "Agua"
-    elif ultD == 4 | 5 :
+    elif ultD == 4 or ultD == 5:
         return "Madera"
-    elif ultD == 6 | 7 :
+    elif ultD == 6 or ultD == 7:
         return "Fuego"
-    elif ultD == 8 | 9 :
+    elif ultD == 8 or ultD == 9:
         return "Tierra"
-
-    elemento = calcular_elemento(anioAc, anioN)
-
 
 
 def generar_prediccion(nombre, elemento, numS):
 
-    nombre = input("Por favor ingrese su nombre: ")
-    numS = int(input("Por favor ingrese su número de la suerte (1, 2, 3, 4): "))
-
     match numS:
         case 1:
-            f"{nombre}, tu conexión con el elemento {elemento} te traera gran sabiduria!.¡Es un buen momento para aprender algo nuevo!"
+            print(f"{nombre}, tu conexión con el elemento {elemento} te traera gran sabiduria!.¡Es un buen momento para aprender algo nuevo!")
         case 2:
-            f"Tu conexión con el elemento {elemento} te representa {nombre}, ya que seguir asi te guiara a un buen futuro."
+            print(f"Tu conexión con el elemento {elemento} te representa {nombre}, ya que seguir asi te guiara a un buen futuro.")
         case 3:
-            f"Asi que tu elemento es {elemento}, que suerte {nombre} ya que significa que te persigue la abundancia!"
+            print(f"Asi que tu elemento es {elemento}, que suerte {nombre} ya que significa que te persigue la abundancia!")
         case 4:
-            f"Enhorabuena {nombre} tu conexión con el elemento {elemento} te dortará con salud."
+            print(f"Enhorabuena {nombre} tu conexión con el elemento {elemento} te dortará con salud.")
+        case _:
+            print("Número de la suerte invalido. Usa 1, 2, 3 o 4.")
 
 
 
@@ -42,11 +38,17 @@ def iniciar_oraculo():
         opcion = input("¿Deseas conocer tu destino?  (si/no) ").lower()
 
         if opcion == 'si':
-            
-            calcular_elemento()
-            generar_prediccion()
+            nombre = input("Por favor ingrese su nombre: ")
+            elemento = calcular_elemento()
+            numS = int(input("Por favor ingrese su número de la suerte (1, 2, 3, 4): "))
+            generar_prediccion(nombre, elemento, numS)
         
         elif opcion == 'no':
             print("Hasta luego.")
             break
+
+        else:
+            print("Opción no valida, escriba 'Si' o 'No'.")
+
+            
 iniciar_oraculo()
