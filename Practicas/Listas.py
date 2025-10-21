2142612b1808b97b771c5aea9707b4185a1ec9c2
Abraham Ruiz <jesus.ruiz279158@potros.itson.edu.mx>
2025-10-21 08:13:45 -0700

    Listas

---
+++ Practicas/Listas.py
@@ -0,0 +1,12 @@
+playlist_musica = [
+    
+    "Bohemian Rhapsody", 
+    "Hotel California", 
+    "Stairway to Heaven"
+]
+playlist_musica.append("Seventh heaven")
+print(playlist_musica)
+playlist_musica[1] = "shape of you"
+playlist_musica.insert(0, "watermelon sugar")
+playlist_musica.pop()
+print(playlist_musica)
